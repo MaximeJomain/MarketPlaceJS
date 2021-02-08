92c8edd08ec77e68ec0354086c11b6911712f58c
Ugo <ugo.prenat@gmail.com>
2021-02-08 15:11:31 +0100

    Story 1 => apparition d'article dans le panier + bouton de suppression

---
+++ js/cart.js
@@ -0,0 +1,25 @@
+
+var addToCartBtn = document.querySelectorAll('.add-to-cart')
+var cart = document.querySelector('#cart tbody')
+
+for (let i = 0; i < addToCartBtn.length; i++) {
+    const btn = addToCartBtn[i];
+    btn.addEventListener('click', () => {
+        let cardId = btn.getAttribute('data-id')
+        addToCart(cardId)
+    })
+}
+
+function addToCart(id) {
+    let course = COURSES[id]
+    console.log(`Add ${course.title} to cart`);
+    cart.insertAdjacentHTML('afterbegin', `
+        <tr>
+            <td><img src="img/courses/${course.img}" alt="${course.title} logo"></td>
+            <td>${course.title}</td>
+            <td>${course.price}€</td>
+            <td>1</td>
+            <td><img src="img/cross.png" class="remove-course" data-id="${id}" style="width:25px;height:auto;cursor:pointer"></td>
+        </tr>
+    `)
+}
