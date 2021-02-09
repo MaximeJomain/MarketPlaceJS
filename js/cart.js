50355baa064b70463b0c16ebd3951079db033da7
Mancone Alexis <ale.mancone@gmail.com>
2021-02-09 14:50:48 +0100

    Story 4 fonction de suppression des cours dans le panier

--- js/cart.js
+++ js/cart.js
@@ -3,27 +3,27 @@ var addToCartBtn    = document.querySelectorAll('.add-to-cart')
 var cart            = document.querySelector('#cart tbody')
 var clearCartBtn    = document.querySelector('#empty-cart')
 var notif           = document.querySelector('.notifs')
+let deletebtnlist = document.querySelectorAll('.remove-course')
 
-// Si le localStorage est vide quand on arrive sur la page, on y crée un tableau vide
+
+
+// If variable doesn't exists, create it
 if (localStorage.getItem('cartStockage') == null) {
     localStorage.setItem('cartStockage', '[]')
 }
 
-// A chaque initialisation de la page, on récupère le contenu du tableau dans une variable objet cartStockage
+// Add courses to cart from localstorage
 var cartStockage = JSON.parse(localStorage.getItem('cartStockage'))
-
-// On réajoute dans le panier tous les objets deja présents dans cartStockage
 for (let i = 0; i < cartStockage.length; i++) {
     addToCart(cartStockage[i])
     updateCourseStock(cartStockage[i])
 }
 
-// Ici, on attends un évenement clic sur les boutons d'ajout au panier, on récupère l'ID du bouton puis on ajoute l'objet dans le panier et dans cartStockage
+// Add courses to cart from html
 for (let i = 0; i < addToCartBtn.length; i++) {
     const btn = addToCartBtn[i];
     btn.addEventListener('click', () => {
         let cardId = btn.getAttribute('data-id')
-
         if (COURSES[cardId].stock > 0) {
             addToCart(cardId)
     
@@ -41,11 +41,8 @@ for (let i = 0; i < addToCartBtn.length; i++) {
     })
 }
 
-// On met a jour le tableau présent dans le localStorage après ajout ou suppression d'objets dans le panier
+// Save cart data in localstorage
 localStorage.setItem('cartStockage', JSON.stringify(cartStockage))
-
-// Put an eventListener on the clear cart btn
-clearCartBtn.addEventListener('click', clearCart)
 
 function addToCart(id) {
     let course = COURSES[id]
@@ -56,15 +53,29 @@ function addToCart(id) {
             <td>${course.title}</td>
             <td>${course.price}€</td>
             <td>1</td>
-            <td><img src="img/cross.png" class="remove-course" data-id="${id}" style="width:25px;height:auto;cursor:pointer"></td>
+            <td><img src="img/cross.png" class="remove-course" data-id="${id}" onclick="removeToCart(${id})" style="width:25px;height:auto;cursor:pointer"></td>
         </tr>
     `)
+    deletebtnlist = document.querySelectorAll('.remove-course')
+
 }
 function updateCourseStock(id) {
     let newStock = COURSES[id].stock -= 1
     let stockSpan = coursesCard[id - 1].querySelector('.stock')
     stockSpan.innerHTML = newStock
 } 
+function removeToCart(id) {
+    cartcourselist = document.querySelectorAll('.cart-course')
+    for (let i = 0; i < cartcourselist.length; i++) {
+        const course = cartcourselist[i];
+        let courseId = course.querySelector('.remove-course').getAttribute('data-id')
+
+        if (courseId == id) {
+            cart.removeChild(cartcourselist[i])
+            localStorage.removeItem(id)
+        }
+    }
+}
 
 function clearCart() {
     var cartCoursesList = document.querySelectorAll('.cart-course')
@@ -87,6 +98,7 @@ function displayNotifAdd(cardId){
     </div>
 `)
     $('.alert').addClass("hide")
+
 }
 
 function displayNotifRemove(cardId){
